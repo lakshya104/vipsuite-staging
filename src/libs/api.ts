3caff1428decdbf7427c79f13c181b74a4ef57c5
lakshyavyas <lakshya.vyas@anktech.co.in>
2024-08-05 12:47:54 +0530

    fixed errors

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -12,12 +12,11 @@ export const loginApi = async ({ username, password }: LoginFormValues) => {
     });
     return response.data;
   } catch (error) {
-    console.error('Error during login:', error);
+    console.error('Error during authentication:', error);
     if (axios.isAxiosError(error)) {
       const errorMessage = error.response?.data?.message || 'An error occurred during login';
-      throw new Error(errorMessage);
+      throw errorMessage;
     }
-    throw new Error('An unexpected error occurred');
   }
 };
 
