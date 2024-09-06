5a93e09644caf656283111a064cac977e4502e8f
lakshyavyas <lakshya.vyas@anktech.co.in>
2024-09-06 19:01:45 +0530

    Styling changes

--- src/app/(home layout)/opportunities/page.tsx
+++ src/app/(home layout)/opportunities/page.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import './opportunities.scss';
 import OpportunitiesPage from '@/site-pages/OpportunitiesPage';
 import OpportunitiesPageLoading from '@/site-pages/OpportunitiesPage/loading';
@@ -8,9 +8,6 @@ export default async function Page() {
   return (
     <Box className="opportunities">
       <Container>
-        <Typography className="page-title" variant="h2" align="center">
-          Opportunities
-        </Typography>
         <Suspense fallback={<OpportunitiesPageLoading />}>
           <OpportunitiesPage />
         </Suspense>
