54fbc6c2263337a658df087b616e68e96619f7ba
lakshyavyas <lakshya.vyas@anktech.co.in>
2024-09-30 17:04:18 +0530

    Test Deployment

--- src/components/MyVipCard.tsx
+++ src/components/MyVipCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { ProgressBarLink } from './ProgressBar';
+// import { ProgressBarLink } from './ProgressBar';
 import { Box, IconButton, Typography } from '@mui/material';
 import Image from 'next/image';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -18,86 +18,83 @@ interface MyVipCardProps {
   vipId: string;
 }
 
-const MyVipCard: React.FC<MyVipCardProps> = ({ image, name, instaFollowers, tiktokFollowers, link, status, vipId }) => {
+const MyVipCard: React.FC<MyVipCardProps> = ({ image, name, instaFollowers, tiktokFollowers, status, vipId }) => {
   const router = useRouter();
   const itemImage = image || '/img/placeholder-image.jpg';
   const handleClick = async (vipId: string) => {
-    if (status !== 'pending') {
-      try {
-        await createVipIdCookie(vipId);
-        await revalidateTag(TAGS.GET_DASHBOARD);
-        await revalidatePathAction(`/agent-home`);
-        router.refresh();
-      } catch (error) {
-        console.error('Error in handleClick:', error);
-      }
+    try {
+      await createVipIdCookie(vipId);
+      await revalidateTag(TAGS.GET_DASHBOARD);
+      await revalidatePathAction(`/agent-home`);
+      router.refresh();
+    } catch (error) {
+      console.error('Error in handleClick:', error);
     }
   };
 
   return (
-    <ProgressBarLink href={link}>
-      <Box
-        onClick={(e) => {
-          e.preventDefault();
-          handleClick(vipId);
-        }}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: '12px',
-          borderBottom: '1px solid #e0e0e0',
-          textDecoration: 'none',
-          color: 'inherit',
-        }}
-      >
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Image
-            src={itemImage}
-            width={100}
-            height={100}
-            alt={name}
-            style={{ width: 80, height: 80, marginRight: 2, borderRadius: '50px', margin: '0 20px 0 0' }}
-          />
-          <Box>
-            <Typography variant="body1" sx={{ fontWeight: 'bold', textTransform: 'capitalize' }}>
-              {name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              <Image
-                src="/img/instagram.svg"
-                width={10}
-                height={10}
-                alt={name}
-                style={{ width: 18, height: 18, marginRight: 10 }}
-              />
-              {instaFollowers}
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              <Image
-                src="/img/tiktok.svg"
-                width={10}
-                height={10}
-                alt={name}
-                style={{ width: 18, height: 18, marginRight: 10 }}
-              />
-              {tiktokFollowers}
-            </Typography>
-            <Typography
-              fontSize="12px"
-              textAlign={'left'}
-              sx={{ fontWeight: 'bold', textTransform: 'capitalize' }}
-              variant="body1"
-            >
-              Status: {status}
-            </Typography>
-          </Box>
+    <Box
+      onClick={() => {
+        handleClick(vipId);
+      }}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '12px',
+        borderBottom: '1px solid #e0e0e0',
+        textDecoration: 'none',
+        color: 'inherit',
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Image
+          src={itemImage}
+          width={100}
+          height={100}
+          alt={name}
+          style={{ width: 80, height: 80, marginRight: 2, borderRadius: '50px', margin: '0 20px 0 0' }}
+        />
+        <Box>
+          <Typography variant="body1" sx={{ fontWeight: 'bold', textTransform: 'capitalize' }}>
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            <Image
+              src="/img/instagram.svg"
+              width={10}
+              height={10}
+              alt={name}
+              style={{ width: 18, height: 18, marginRight: 10 }}
+            />
+            {instaFollowers}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            <Image
+              src="/img/tiktok.svg"
+              width={10}
+              height={10}
+              alt={name}
+              style={{ width: 18, height: 18, marginRight: 10 }}
+            />
+            {tiktokFollowers}
+          </Typography>
+          <Typography
+            fontSize="12px"
+            textAlign={'left'}
+            sx={{ fontWeight: 'bold', textTransform: 'capitalize' }}
+            variant="body1"
+          >
+            Status: {status}
+          </Typography>
         </Box>
-        <IconButton>
-          <ArrowForwardIosIcon color="primary" />
-        </IconButton>
       </Box>
-    </ProgressBarLink>
+      {/* <ProgressBarLink href={link}> */}
+      <IconButton>
+        <ArrowForwardIosIcon color="primary" />
+      </IconButton>
+      {/* </ProgressBarLink> */}
+    </Box>
   );
 };
 
