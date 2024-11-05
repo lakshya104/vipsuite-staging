c6495054c769fbdbe93541e31f3e0449fd981e34
lakshyavyas <lakshya.vyas@anktech.co.in>
2024-11-05 18:29:19 +0530

    Lint Fix

--- src/site-pages/InboxPage/index.tsx
+++ src/site-pages/InboxPage/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { GetAllOrders, GetNonce } from '@/libs/api-manager/manager';;
+import { GetAllOrders, GetNonce } from '@/libs/api-manager/manager';
 import InboxTabs from '@/components/InboxTabs/InboxTabs';
 
 const InboxPage = async () => {
   const nonce = await GetNonce();
   const allOrders = await GetAllOrders(nonce);
-  return <InboxTabs order={allOrders} />;
+  return <InboxTabs order={allOrders} />
 };
 
 export default InboxPage;
