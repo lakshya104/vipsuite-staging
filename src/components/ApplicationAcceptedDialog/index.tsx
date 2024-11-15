d3551ce565fcae75f101cfaac14cdf0b73de9b70
lakshyavyas <lakshya.vyas@anktech.co.in>
2024-11-15 15:00:36 +0530

    Type Fix

--- src/components/ApplicationAcceptedDialog/index.tsx
+++ src/components/ApplicationAcceptedDialog/index.tsx
@@ -5,7 +5,7 @@ import { Backdrop, Box, Button, CircularProgress, Typography } from '@mui/materi
 import en from '@/helpers/lang';
 import './ApplicationAcceptedDialog.scss';
 import Image from 'next/image';
-import '../../app/(auth)/on-boarding/style.scss'
+import '../../app/(auth)/on-boarding/style.scss';
 
 const ApplicationAcceptedDialog = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -22,27 +22,28 @@ const ApplicationAcceptedDialog = () => {
   return (
     <>
       <Box className="bg-textBlack application__status onboarding__page">
-         <Box flexGrow={1} className="onboarding__page-inner">
-            <Box className="onboarding__logo">
-              <Typography variant="h1">{en.customAcceptedScreen.acceptedTxt}</Typography>
-            </Box>
-            <Box className="application__status-content">
-              <Image
-                alt="Application Approved"
-                src="/img/application-status.png"
-                width={199} height={199}
-                sizes="(max-width: 300px) 100vw, 300px"
-                style={{ objectFit: 'contain' }}
-                priority
-              />
-              <Typography variant="h3">{en.customAcceptedScreen.welcomeTxt}</Typography>
-              <Typography variant="body1">{en.customAcceptedScreen.welcomePara}</Typography>
-            </Box>
-            <Box className="onboarding__page-links">
-              <Button className="onboarding__link button button--white" onClick={handleAcceptDialogChange}>
-                {en.customAcceptedScreen.continue}
-              </Button>
-            </Box>
+        <Box flexGrow={1} className="onboarding__page-inner">
+          <Box className="onboarding__logo">
+            <Typography variant="h1">{en.customAcceptedScreen.acceptedTxt}</Typography>
+          </Box>
+          <Box className="application__status-content">
+            <Image
+              alt="Application Approved"
+              src="/img/application-status.png"
+              width={199}
+              height={199}
+              sizes="(max-width: 300px) 100vw, 300px"
+              style={{ objectFit: 'contain' }}
+              priority
+            />
+            <Typography variant="h3">{en.customAcceptedScreen.welcomeTxt}</Typography>
+            <Typography variant="body1">{en.customAcceptedScreen.welcomePara}</Typography>
+          </Box>
+          <Box className="onboarding__page-links">
+            <Button className="onboarding__link button button--white" onClick={handleAcceptDialogChange}>
+              {en.customAcceptedScreen.continue}
+            </Button>
+          </Box>
         </Box>
       </Box>
       <Backdrop sx={{ color: 'white', zIndex: 100 }} open={loading}>
--- src/components/ApplicationReviewDialog/index.tsx
+++ src/components/ApplicationReviewDialog/index.tsx
@@ -4,8 +4,8 @@ import { Backdrop, Box, Button, CircularProgress, Typography } from '@mui/materi
 import { LogOut } from '@/libs/api-manager/manager';
 import { signOut } from 'next-auth/react';
 import en from '@/helpers/lang';
-import '../../app/(auth)/on-boarding/style.scss'
-import './ApplicationReviewDialog.scss'
+import '../../app/(auth)/on-boarding/style.scss';
+import './ApplicationReviewDialog.scss';
 
 const ApplicationReviewDialog = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -23,20 +23,20 @@ const ApplicationReviewDialog = () => {
   return (
     <>
       <Box className="bg-textBlack application__review onboarding__page">
-         <Box flexGrow={1} className="onboarding__page-inner">
-            <Box className="onboarding__logo">
-              <Typography variant="h1">{en.customProfileInReviewScreen.thankyou}</Typography>
-            </Box>
-            <Box className="application__review-content">
-              <Typography variant="h3">{en.customProfileInReviewScreen.inReview}</Typography>
-              <Typography variant="body1">{en.customProfileInReviewScreen.inReviewPara}</Typography>
-              <Typography variant="body1">{en.customProfileInReviewScreen.inReviewSubPara}</Typography>
-            </Box>
-            <Box className="onboarding__page-links">
-              <Button className="onboarding__link button button--white" onClick={handleReviewDialogChange}>
-                {en.customProfileInReviewScreen.done}
-              </Button>
-            </Box>
+        <Box flexGrow={1} className="onboarding__page-inner">
+          <Box className="onboarding__logo">
+            <Typography variant="h1">{en.customProfileInReviewScreen.thankyou}</Typography>
+          </Box>
+          <Box className="application__review-content">
+            <Typography variant="h3">{en.customProfileInReviewScreen.inReview}</Typography>
+            <Typography variant="body1">{en.customProfileInReviewScreen.inReviewPara}</Typography>
+            <Typography variant="body1">{en.customProfileInReviewScreen.inReviewSubPara}</Typography>
+          </Box>
+          <Box className="onboarding__page-links">
+            <Button className="onboarding__link button button--white" onClick={handleReviewDialogChange}>
+              {en.customProfileInReviewScreen.done}
+            </Button>
+          </Box>
         </Box>
       </Box>
       <Backdrop sx={{ color: '#fff', zIndex: 100 }} open={loading}>
