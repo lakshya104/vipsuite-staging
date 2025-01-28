46e6884b488f060f907f8b9cfbdf6133d1b0ad07
lakshyavyas <lakshya.vyas@anktech.co.in>
2025-01-28 12:45:56 +0530

    .

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+  },
+};
+
+export default nextConfig;
