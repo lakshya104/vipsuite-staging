184578cdad3bdcbf76c7b24ee6d0affa6ca277dc
lakshyavyas <lakshya.vyas@anktech.co.in>
2025-09-23 11:59:46 +0530

    Css check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
       </head>
       <body>
         <AppRouterCacheProvider>
